e8de6f7fd4765b35aea2941790002ee28bcb00ef
taipon55 <Taisei127@outlook.com>
2023-01-07 07:21:34 +0900

    vuetify導入

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
+  <v-app>
+    <v-app-bar app color="blue" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title >TodoList</v-app-bar-title>
+    </v-app-bar>
+  <div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList title="TodoList" />
   </div>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoList
+}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>hello</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    title: String,
+  },
+};
+</script>
